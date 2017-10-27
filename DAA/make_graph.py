495c5c18250972d4c76ab81c92bb8df56e99f2cf
Pradeep Kumar <pradeepisro49@gmail.com>
2017-10-27 10:47:28 +0530

    Updated json to plain text for graph input

--- DAA/make_graph.py
+++ DAA/make_graph.py
@@ -1,15 +1,23 @@
 import json
 import pprint
 
-file = open("./bellmanFord.json")
-output = open("./bellmanFord.txt", "w")
+file = open("./input/bellmanFord.json")
+output = open("./input/bellmanFord.txt", "w")
 getstr = file.read()
 getjson = json.loads(getstr)
+nedges = 0
 pprint.pprint(getjson)
 output.writelines(str(getjson["n"]) + "\n")
+
+for keys in getjson["graph"]:
+    nedges = nedges + len(getjson["graph"][keys])
+
+output.writelines(str(nedges) + "\n")
+
 for i in getjson["graph"]:
     output.write(i + " ")
     output.write(str(len(getjson["graph"][i])) + " ")
     for j in getjson["graph"][i]:
+        nedges = nedges + 1
         output.write("{} {} ".format(j[0], j[1]))
     output.write("\n")
