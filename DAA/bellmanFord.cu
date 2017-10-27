189e70b77165e7045303c331d302fb4bc07af392
Pradeep Kumar <pradeepisro49@gmail.com>
2017-10-27 10:51:10 +0530

    Updated my package source files

--- DAA/bellmanFord.cu
+++ DAA/bellmanFord.cu
@@ -1,5 +1,8 @@
 #include "bellmanFord.hpp"
 
+__constant__ globalParams gpuParams;
+
+
 cudaError_h::cudaError_h(cudaError_t err){
     if(err != cudaSuccess)
         printf("%s in file %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
@@ -7,7 +10,7 @@ cudaError_h::cudaError_h(cudaError_t err){
 
 bellmanFord::bellmanFord(std::string filename){
     fin.open(filename.c_str());
-    if(fin.fail()){
+    if(fin.fail() || filename == ""){
         std::cout << "unable to open the file!\n";
         std::exit(0);
     }
@@ -18,6 +21,9 @@ bellmanFord::bellmanFord(std::string filename){
     this->h_distance = new int[nNodes];
     this->h_pi = new int[nNodes];
     this->h_index = new int[nNodes];
+    this->h_queue_1 = new bool[nNodes];
+    this->h_queue_2 = new bool[nNodes];
+
     this->h_edge = new int[nEdges];
     this->h_weight = new int[nEdges];
 
@@ -26,6 +32,8 @@ bellmanFord::bellmanFord(std::string filename){
     for(int i = 0, j = 0; i < nNodes; i++){
         fin >> current_node;
         fin >> current_count;
+        this->h_queue_1[i] = false;
+        this->h_queue_2[i] = false;
         this->h_pi[i] = -1;
         this->h_distance[i] = 100000;
         for(j = 0; j < current_count; j++){
@@ -37,7 +45,7 @@ bellmanFord::bellmanFord(std::string filename){
     }
 }
 
-int bellmanFord::gpuInit(){
+int bellmanFord::gpuInit(int source){
 
     cudaDeviceProp prop;
     int dev_count = 0;
@@ -51,6 +59,9 @@ int bellmanFord::gpuInit(){
         std::cout << "Compute Capability: " << prop.major << std::endl;
         std::cout << "----------------------------------------------\n";
     }
+
+    h_queue_1[source] = true;
+    h_distance[source] = 0;
 
     cudaError_h(cudaMalloc((void**)&this->d_index, this->nNodes * sizeof(int)));
     cudaError_h(cudaMemcpy(this->d_index, this->h_index, this->nNodes * sizeof(int), cudaMemcpyHostToDevice));
@@ -67,6 +78,20 @@ int bellmanFord::gpuInit(){
     cudaError_h(cudaMalloc((void**)&this->d_weight, this->nEdges * sizeof(int)));
     cudaError_h(cudaMemcpy(this->d_weight, this->h_weight, this->nEdges * sizeof(int), cudaMemcpyHostToDevice));
 
+    cudaError_h(cudaMalloc((void**)&this->d_queue_1, this->nEdges * sizeof(bool)));
+    cudaError_h(cudaMemcpy(this->d_queue_1, this->h_queue_1, this->nNodes * sizeof(bool), cudaMemcpyHostToDevice));
+
+    cudaError_h(cudaMalloc((void**)&this->d_queue_2, this->nEdges * sizeof(bool)));
+    cudaError_h(cudaMemcpy(this->d_queue_2, this->h_queue_2, this->nNodes * sizeof(bool), cudaMemcpyHostToDevice));
+
+    gpuParams.nNodes = nNodes;
+    gpuParams.nEdges = nEdges;
+    gpuParams.index = d_index;
+    gpuParams.edge = d_edge;
+    gpuParams.weigth = d_weight;
+    gpuParams.f1 = d_queue_1;
+    gpuParams.f2 = d_queue_2;
+
     return 0;
 }
 
@@ -75,11 +100,36 @@ void bellmanFord::displayPrecedence(){
     for(int i = 0; i < this->nNodes; i++){
         std::cout << i << " " << this->h_pi[i] << "\n";
     }
+}
+
+__device__
+void relax(int count){
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if(tid > count){
+        return;
+    }
+}
+
+__device__
+void computeShortestPath(){
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if(tid > gpuParams.nNodes){
+        return;
+    }
+
+    __shared__ bool toggle;
+    toggle = false;
+
+    while(!toggle){
+        if(gpuParams.f1[tid]){
+            
+        }
+    }
 
 }
 
 void bellmanFord::shortestPath(int source){
     h_distance[source] = 0;
-    this->gpuInit();
-    
+    this->gpuInit(source);
+    //computeShortestPath<<<1, nNodes>>>()
 }
--- DAA/bellmanFord.hpp
+++ DAA/bellmanFord.hpp
@@ -19,17 +19,28 @@ public:
     cudaError_h(cudaError_t err);
 };
 
+struct globalParams{
+    int nNodes;
+    int nEdges;
+    int *index, *edge, *weigth;
+    bool *f1, *f2;
+};
+
+extern __constant__ globalParams gpuParams;
+
 class bellmanFord{
 public:
     bellmanFord(std::string);
-    int gpuInit();
-    void shortestPath(int source);
+    int gpuInit(int);
+    void shortestPath(int);
     void displayPrecedence();
 private:
     int *h_index, *h_edge, *h_weight;
     int *h_distance, *h_pi;
+    bool *h_queue_1, *h_queue_2;
     int *d_index, *d_edge, *d_weight;
     int* d_distance, *d_pi;
+    bool *d_queue_1, *d_queue_2;
     std::ifstream fin;
     int nNodes;
     int nEdges;
